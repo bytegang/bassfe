ea29b9c63f904b10e25297844685473843e93d9e
EricZhou <zhouqing1@360.cn>
2020-08-03 19:09:39 +0800

    rename alert

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import { store } from './store'
-import AlertComponent from './components/Shared/Alert.vue'
+import SharedAlert from './components/Shared/Alert.vue'
 
 Vue.config.productionTip = false
-Vue.component('app-alert', AlertComponent)
+Vue.component('app-alert', SharedAlert)
 
 new Vue({
   vuetify,
